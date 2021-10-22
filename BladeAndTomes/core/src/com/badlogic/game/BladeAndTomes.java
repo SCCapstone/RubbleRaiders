05eb4eb43d4271fa52e001c6af820ea5c246cc34
atomicmanx1 <akemmons@email.sc.edu>
2021-10-22 19:02:21 -0400

    Added application title + changed out unneeded asset in default code to prevent errors.

--- BladeAndTomes/core/src/com/badlogic/game/BladeAndTomes.java
+++ BladeAndTomes/core/src/com/badlogic/game/BladeAndTomes.java
@@ -1,23 +1,23 @@
 package com.badlogic.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class BladeAndTomes extends ApplicationAdapter {
+public class BladeAndTomes extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("PlayerIcon.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0.1f, 0.2f, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
