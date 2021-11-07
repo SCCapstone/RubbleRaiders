bd7ca56d43df304de8c1363836a46ecd1f445edc
V.N. Anirudh Oruganti <oruganti@email.sc.edu>
2021-11-07 17:58:28 -0500

    Made ButtonClick Sound Class, Used Sources: https://www.youtube.com/watch?v=MsDENED-sZw&t=171s

--- BladeAndTomes/core/src/Sounds/ButtonClickSound.java
+++ BladeAndTomes/core/src/Sounds/ButtonClickSound.java
@@ -1,7 +1,39 @@
 package Sounds;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class ButtonClickSound {
-    public ButtonClickSound (){
+    private float volume;
+    private float pitch;
+    private boolean isMusted;
+    private String buttonClickPath;
+    private Sound button;
+    long id;
+    public ButtonClickSound (float customVolume, float customPitch, boolean muted, String customButtonClickPath){
+            volume = customVolume;
+            pitch = customPitch;
+            isMusted = muted;
+            buttonClickPath = customButtonClickPath;
+    }
+    public ButtonClickSound(){
+        volume = 0.5f;
+        pitch = 2f;
+        isMusted = false;
+        buttonClickPath = "buttonClick.wav";
+    }
+    public void setVolume(float newVolume){
+        volume = newVolume;
+    }
+    public void setMute(boolean mute){
+        isMusted = mute;
+    }
+    public void playClick(){
+        button = Gdx.audio.newSound(Gdx.files.internal(buttonClickPath));
+        id = button.play(volume);
+        button.setPitch(id,pitch);
+        button.setLooping(id,false);
 
     }
+
 }
