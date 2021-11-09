15d66f9935d1868cb5e32b390464c7128c2bb813
atomicmanx1 <akemmons@email.sc.edu>
2021-11-09 13:48:53 -0500

    Fixing a few issues in NPC class.

---
+++ BladeAndTomes/core/src/com/badlogic/game/creatures/Npc.java
@@ -0,0 +1,32 @@
+package com.badlogic.game.creatures;
+
+import com.badlogic.game.creatures.Entity;
+
+public class Npc extends Entity {
+
+    private enum NpcClass {Trader, Companion, Enemy};
+
+    private int npc;
+
+    public Npc()
+    {
+        super();
+    }
+
+    public Npc(int healthPoints, int armorPoints, int movement, int npcClass)
+    {
+        super(healthPoints, armorPoints, movement);
+        this.npc = npcClass;
+    }
+
+    public int getNpcClass()
+    {
+        return npc;
+    }
+
+    public void setNpcClass(int npcClass)
+    {
+        this.npc = npcClass;
+    }
+
+}
