099a6a96722cc364623b99e50ef7f7df9ce711ff
Anirudh Oruganti <oruganti@email.sc.edu>
2022-03-04 14:57:01 -0500

    Added Fading to Progress Bar

--- BladeAndTomes/core/src/ScreenOverlayRework/Health/Health.java
+++ BladeAndTomes/core/src/ScreenOverlayRework/Health/Health.java
@@ -2,6 +2,7 @@ package ScreenOverlayRework.Health;
 
 import com.badlogic.game.BladeAndTomes;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
@@ -18,20 +19,33 @@ public class Health {
     private TextureAtlas healthBarAtlas;
     private Skin healthBarSkin;
     private Group group;
+    private Color change;
     public Health(BladeAndTomes game)  {
         this.game = game;
         healthBarAtlas = new TextureAtlas(Gdx.files.internal("SkinAssets/ProgressBar/healthbarUI.atlas"));
         healthBarSkin = new Skin((Gdx.files.internal("SkinAssets/ProgressBar/healthbarUI.json")),healthBarAtlas);
-        healthBar = new ProgressBar(0,10,1,false,healthBarSkin);
+        healthBar = new ProgressBar(0,10,0.5f,false,healthBarSkin);
         healthBar.setValue(game.player.getHealthPoints());
 
         group = new Stack();
         group.setBounds(game.stageInstance.getWidth()/1.3f, game.stageInstance.getHeight()/2.3f,
                 game.stageInstance.getWidth()/10f, game.stageInstance.getHeight());
+        change = new Color();
+        ColorFade();
+        healthBar.setColor(change);
         group.addActor(healthBar);
 
     }
+
+    public void ColorFade(){
+        float red   = (game.player.getHealthPoints()) <= 5 ? (float) Math.pow(2, (game.player.getHealthPoints())) : 0;
+        float green = (game.player.getHealthPoints()) > 5 ? (float) Math.pow(3, (game.player.getHealthPoints())) : 0;
+        change.set(red,green ,0,0.5f);
+
+    }
     public void update() {
+        ColorFade();
+        healthBar.setColor(change);
         healthBar.setValue(game.player.getHealthPoints());
     }
     public Group getHealthBar() {
