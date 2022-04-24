873bcb0bbcfcab236607217e93473669409fb527
anriudh oruganti <oruganti@email.sc.edu>
2022-04-24 19:31:40 -0400

    Added bcgnd

--- BladeAndTomes/core/src/com/badlogic/game/screens/CharacterCreation.java
+++ BladeAndTomes/core/src/com/badlogic/game/screens/CharacterCreation.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
@@ -52,7 +54,9 @@ public class CharacterCreation extends ScreenAdapter {
         //Instance for the game
         this.GAME = game;
         batch = new SpriteBatch();
-
+        Texture background = new Texture(Gdx.files.internal("Settings.jpg"));
+        Image backgroundImage = new Image(background);
+        game.stageInstance.addActor(backgroundImage);
         //set initial values
         points = 9;
         primaryPoints = new Label("Points: "+points, game.generalLabelStyle);
